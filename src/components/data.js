b5eda42068e2bcbe7a955979de9ee7d247e1bf2e
hcbarros <hcb@a.recife.ifpe.edu.br>
2021-04-17 16:55:51 -0300

    commit

---
+++ src/components/data.js
@@ -0,0 +1,16 @@
+
+
+export const arrayColors = ['Amarelo','Azul','Branco','Cinza','Laranja',
+                            'Verde','Vermelho','Preto','Rosa','Vinho'];
+
+
+export const arraySizes = ['P','M','G','GG','U','36','38','40','42','44','46'];
+
+
+export const arrayPrices = ['de R$0 até R$50','de R$51 até R$150','de R$151 até R$300',
+                            'de R$301 até R$500','a partir de R$501'];
+
+
+export const arrayChecked = [false,false,false,false,false,
+                             false,false,false,false,false,false];
+
--- src/screens/main.js
+++ src/screens/main.js
@@ -5,6 +5,8 @@ import './checkbox.css';
 import React, { useState, useEffect } from 'react';
 import { useStateWithCallbackLazy } from 'use-state-with-callback';
 import $ from 'jquery';
+import { arrayColors, arraySizes, arrayPrices, 
+         arrayChecked } from '../components/data';
 import logo from '../images/logo-m3.png';
 import bag from '../images/bag.png';
 
@@ -13,7 +15,8 @@ export default function Main() {
 
      const [verCores, setVerCores] = useState(false);   
      const [btnArray, setBtnArray] = useState([]);
-     const [gridDisabled, setGridDisabled] = useStateWithCallbackLazy([]);
+     const [gridChecked, setGridChecked] = useStateWithCallbackLazy(arrayChecked);
+
 
 
     $('.js-dropp-action').on("click", function(e) {
@@ -34,42 +37,41 @@ export default function Main() {
       });
 
 
-    const checkAction = (evt, grid, text, index) => {
+    const checkAction = (evt) => {
+  
+        const obj = JSON.parse(evt.target.value);
 
-        const disabled = evt.target.disabled;
-        evt.target.disabled = true;  
-        if(grid) {
-            let array = gridDisabled;
-            if(array.length === 0) {
-                ['','','','','','','','','','','',]
-                .map(x => array.push(true));
-                setGridDisabled(array, () => {});
-            }    
-            array[index] = disabled;
-            setGridDisabled(array, () => {});
+        if(obj.sizes) {
+            let array = gridChecked;
+                
+            array[obj.index] = evt.target.checked;
+            setGridChecked(array, () => {});
+
         }
-
+        
         setBtnArray([], () => {});
 
-        evt.target.disabled = false;  
     }
 
 
-    const displayChecks = (array, drop, grid) => {
+    const displayChecks = (array, obj) => {
 
-        return array.map((text, index) => 
-            <div className={drop ? "line-check check-drop" : "line-check"}>
-                <label class={grid ? "container-check-grid" : "container-check"}>
-                    <input type="checkbox" 
-                    onClick={(evt) => checkAction(evt, grid, text, index)} />
-                    <span class={!grid ? "checkmark" : gridDisabled[index] ?  
-                    "checkmark-grid" : "checkmark-grid-active"}>
-                        {grid && text}
-                    </span>
-                </label>    {!grid && <div>{text}</div>}
-            </div>)  
+        return array.map((text, index) => {
+            
+            obj.index = index;
+            obj.text = text;
+            return <div className={obj.drop ? "line-check check-drop" : "line-check"}>
+                        <label class={obj.sizes ? "container-check-grid" : "container-check"}>                        
+                            <input type="checkbox" value={JSON.stringify(obj)}
+                            onClick={(evt) => checkAction(evt)} />
+                            <span class={!obj.sizes ? "checkmark" : gridChecked[index] ?  
+                            "checkmark-grid-active" : "checkmark-grid"}>
+                                {obj.sizes && text}
+                            </span>
+                        </label>    {!obj.sizes && <div>{text}</div>}
+                    </div>
+        });  
     }  
-
 
 
     return (
@@ -87,25 +89,24 @@ export default function Main() {
                 <div className="filters">
 
                     <div className="text-clothers">Blusas</div>
-                    <div className="text-filter">CORES</div>
+                    <div className="text-colors">CORES</div>
                     
-                    {displayChecks(['Amarelo','Azul','Branco','Cinza','Laranja'], false, false)}
+                    {displayChecks(arrayColors.slice(0,5), {colors: true})}
 
-                    {verCores && displayChecks(['Verde','Vermelho','Preto','Rosa','Vinho'], true, false)}
+                    {verCores && displayChecks(arrayColors.slice(5,10), {drop: true, colors: true})}
 
                     <div className="text-ver-cores" onClick={() => setVerCores(!verCores)}>
                         {verCores ? "Esconder últimas cores" : "Ver todas as cores"} 
                     <div className={verCores ? "arrow-down" : "arrow-up"}></div></div>
                     
 
-                    <div className="text-tamanhos">TAMANHOS</div>
+                    <div className="text-sizes">TAMANHOS</div>
 
-                    {<div className="wrapper">
-                        {displayChecks(['P','M','G','GG','U','36','38','40','42','44','46'], 
-                            false, true)}                        
-                    </div> }
+                    {<div className="wrapper"> {displayChecks(arraySizes, {sizes: true})} </div> }
 
+                    <div className="text-prices">FAIXA DE PREÇO</div>
 
+                    {displayChecks(arrayPrices.slice(0,5), {prices: true})}
 
                 </div>
 
