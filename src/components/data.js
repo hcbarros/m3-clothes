9561d4305933246373ab250707cbb7570788a1db
hcbarros <hcb@a.recife.ifpe.edu.br>
2021-04-17 21:27:21 -0300

    commit

--- src/components/data.js
+++ src/components/data.js
@@ -1,4 +1,14 @@
 
+import image2 from '../images/img_2.png';
+import image3 from '../images/img_3.png';
+import image4 from '../images/img_4.png';
+import image5 from '../images/img_5.png';
+import image6 from '../images/img_6.png';
+import image7 from '../images/img_7.png';
+import image8 from '../images/img_8.png';
+import image9 from '../images/img_9.png';
+import image10 from '../images/img_10.png';
+
 
 export const arrayColors = ['Amarelo','Azul','Branco','Cinza','Laranja',
                             'Verde','Vermelho','Preto','Rosa','Vinho'];
@@ -14,3 +24,90 @@ export const arrayPrices = ['de R$0 até R$50','de R$51 até R$150','de R$151 at
 export const arrayChecked = [false,false,false,false,false,
                              false,false,false,false,false,false];
 
+
+export const arrayClothers = [
+
+            {
+                image: image2,
+                description: 'CAMISETA MESCLA',
+                price: 'R$ 28,00',
+                promotion: 'até 3x de R$9,33',    
+                colors: ['Cinza','Laranja','Vermelho'],
+                sizes: ['M','U','36','42','46'],                
+                range: 0
+            },
+
+            {
+                image: image3,
+                description: 'SAIA EM COURO',
+                price: 'R$ 398,00',
+                promotion: 'até 5x de R$30,00',    
+                colors: ['Rosa','Laranja','Vermelho','AZUL'],
+                sizes: ['P','G','GG','40','42'],                
+                range: 3    
+            },
+            {
+                image: image4,
+                description: 'CARDIGAN TIGRE',
+                price: 'R$ 398,00',
+                promotion: 'até 5x de R$30,00',    
+                colors: ['Amarelo','Laranja','Verde','Vinho'],
+                sizes: ['P','M','36','42'],                
+                range: 3    
+            },
+            {
+                image: image5,
+                description: 'CARDIGAN OFF WHITE',
+                price: 'R$ 99,90',
+                promotion: 'até 3x de R$33,30',    
+                colors: ['Cinza','Azul','Branco','Preto'],
+                sizes: ['G','GG','44','46'],                
+                range: 1    
+            },
+            {
+                image: image6,
+                description: 'BODY LEOPARDO',
+                price: 'R$ 129,90',
+                promotion: 'até 3x de R$43,43',    
+                colors: ['Vermelho','Rosa','Laranja','Vinho'],
+                sizes: ['P','U','36','38'],                
+                range: 1    
+            },
+            {
+                image: image7,
+                description: 'CASACO PELOS',
+                price: 'R$ 398,00',
+                promotion: 'até 5x de R$30,00',    
+                colors: ['Verde','Azul','Branco','Vinho'],
+                sizes: ['G','40','44'],                
+                range: 3    
+            },
+            {
+                image: image8,
+                description: 'CROPPED STRIPES',
+                price: 'R$ 120,00',
+                promotion: 'até 3x de R$40,00',    
+                colors: ['Amarelo','Branco','Rosa'],
+                sizes: ['P','GG','U'],                
+                range: 1    
+            },
+            {
+                image: image9,
+                description: 'CAMISA TRANSPARENTE',
+                price: 'R$ 398,00',
+                promotion: 'até 5x de R$30,00',    
+                colors: ['Azul','Cinza','Preto','Vinho'],
+                sizes: ['G','U','38','44'],                
+                range: 3    
+            },
+            {
+                image: image10,
+                description: 'POCHETE CLUTCH',
+                price: '99,00',
+                promotion: 'até 3x de R$33,00',    
+                colors: ['Azul','Cinza','Preto','Vinho'],
+                sizes: ['M','U','38','40'],                
+                range: 1    
+            }
+
+]
